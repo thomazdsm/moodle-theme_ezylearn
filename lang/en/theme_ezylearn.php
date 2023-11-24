a35124fd0a9189f4bbd89038fd42f32a1680cd22
Thomaz Machado <thomaz.machado@uemanet.uema.br>
2023-11-24 16:18:37 -0300

    first commit

---
+++ lang/en/theme_ezylearn.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * EzyLearn.
+ *
+ * @package    theme_ezylearn
+ * @copyright  2023 Thomaz Machado - https://xfera.tech
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+// A description shown in the admin theme selector.
+$string['choosereadme'] = 'Theme photo is a child theme of Boost. It adds the ability to upload background photos.';
+// The name of our plugin.
+$string['pluginname'] = 'EzyLearn';
+// We need to include a lang string for each block region.
+$string['region-side-pre'] = 'Right';
